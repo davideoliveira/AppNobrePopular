40c41bf82117b796aae485779797909cf34e1be0
davioliveira <davidomingos816@gmail.com>
2023-04-12 16:44:00 -0300

    criação da tela perfilScreen

--- AppNobrePopular/src/screens/PerfilScreen.js
+++ AppNobrePopular/src/screens/PerfilScreen.js
@@ -1,11 +1,24 @@
 import react from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
 
 
 
 export default function PerfilScreen() {
   return (
-    <View>
+    <View style={styles.container}>
+
+        <View style={styles.box}>
+
+            <View style={styles.iconperson}>
+              <ImageBackground source={require('../../assets/imgs/logoNobrePopular1.jpg')}/>
+            </View>
+
+            <View style={styles.info}>
+
+            </View>
+
+
+        </View>
         <Text>Tela de Perfil</Text>
     </View>
   );
@@ -18,4 +31,29 @@ const styles = StyleSheet.create({
         alignItems: 'center'
 
     },
+    box: {
+      backgroundColor: '#A9A9A9',
+      height: '100%',
+      width: '100%',
+      alignItems: 'center',
+      
+    },
+    iconperson: {
+      marginTop: 20,
+      borderRadius: '50%',
+      borderWidth: 2,
+      // backgroundColor: 'orange',
+      height: 100,
+      width: 100,
+    },
+    info: {
+      marginTop: 20,
+      backgroundColor: 'white',
+      height: '100%',
+      width: '100%',
+      borderTopEndRadius: 25,
+      borderTopStartRadius: 25
+
+
+    }
 })
